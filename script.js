8f816a9674f4910b78c52a1890b513c47150c156
json231 <122293323+json231@users.noreply.github.com>
2023-06-02 14:05:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+ let rapBtn = document.getElementById("rap-btn");
+rapBtn.addEventListener("click", function() {
+  window.location.href = "https://album-suggestions-part-2.jasonguevara3.repl.co";
+});
+
+let hipHopBtn = document.getElementById("hip-hop-btn");
+hipHopBtn.addEventListener("click", function() {
+  window.location.href = "https://album-suggestions-pt3.jasonguevara3.repl.co/";
+});
+
+let latinoBtn = document.getElementById("latino-btn");
+latinoBtn.addEventListener("click", function() {
+  window.location.href = "https://album-suggestions-pt4.jasonguevara3.repl.co/";
+});
+
+let rockBtn = document.getElementById("rock-btn");
+rockBtn.addEventListener("click", function() {
+  window.location.href = "https://album-suggestions-pt5.jasonguevara3.repl.co/";
+});
